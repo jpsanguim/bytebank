576224356116d4d893af596a271c7b2c60e413ff
Jean Sanguim <jpsanguim@outlook.com>
2022-07-11 16:15:27 -0300

    Criação de novos métodos

--- src/Conta.java
+++ src/Conta.java
@@ -2,4 +2,26 @@ public class Conta {
     double saldo;
     int agencia, numero;
     String titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
